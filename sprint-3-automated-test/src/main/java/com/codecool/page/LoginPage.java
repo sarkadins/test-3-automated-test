2b25d2406c3d55f7be811b0239562286d2bcec5b
sarkadins <soma.sarkadi@gmail.com>
2024-05-27 15:57:26 +0200

    implement basepage, homepage, loginpage classes implement logintest class set up enviromental variables

---
+++ sprint-3-automated-test/src/main/java/com/codecool/page/LoginPage.java
@@ -0,0 +1,32 @@
+package com.codecool.page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage {
+
+    String userName = System.getenv("USER_NAME");
+    String password = System.getenv("PASSWORD");
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(id = "login-form-username")
+    WebElement userNameField;
+    @FindBy(id = "login-form-password")
+    WebElement passwordField;
+    @FindBy(id = "login-form-submit")
+    WebElement loginButton;
+
+    public void testLogin(){
+        userNameField.click();
+        userNameField.clear();
+        userNameField.sendKeys(userName);
+        passwordField.click();
+        passwordField.clear();
+        passwordField.sendKeys(password);
+        loginButton.click();
+    }
+}
