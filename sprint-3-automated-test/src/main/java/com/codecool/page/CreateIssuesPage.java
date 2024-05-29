c3ff281611c4d382994bcc35aae1813d42429d70
bftsgt <bfutasz@gmail.com>
2024-05-29 14:53:59 +0200

    committing changes made on main branch

--- sprint-3-automated-test/src/main/java/com/codecool/page/CreateIssuesPage.java
+++ sprint-3-automated-test/src/main/java/com/codecool/page/CreateIssuesPage.java
@@ -1,2 +1,33 @@
-package com.codecool.page;public class CreateIssuesPage {
+package com.codecool.page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class CreateIssuesPage extends BasePage {
+    //https://jira-auto.codecool.metastage.net/secure/CreateIssue!default.jspa
+
+    @FindBy(xpath = "//h1[text()='Create Issue']")
+    WebElement h1ByText;
+
+    @FindBy(id = "issue-create-submit")
+    WebElement nextBtn;
+
+    @FindBy(id = "issue-create-cancel")
+    WebElement cancelBtn;
+
+    @FindBy(id = "project-single-select")
+    WebElement projectDropdown;
+
+    @FindBy(id = "issuetype-single-select")
+    WebElement issueDropdown;
+
+    public CreateIssuesPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public boolean isCreateIssueH1Displayed(){
+        return h1ByText.isDisplayed();
+    }
+
 }
--- sprint-3-automated-test/src/test/java/test/CreateIssuesPageTest.java
+++ sprint-3-automated-test/src/test/java/test/CreateIssuesPageTest.java
@@ -1,5 +1,43 @@
 package test;
 
-public class CreateIssuesTest {
-    
+import com.codecool.page.CreateIssuesPage;
+import com.codecool.page.HomePage;
+import com.codecool.page.LoginPage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CreateIssuesPageTest {
+
+    WebDriver driver;
+    LoginPage loginPage;
+    HomePage homePage;
+    CreateIssuesPage createIssuesPage;
+    private String LOGIN_URL = System.getenv("BASE_URL") + "login.jsp";
+    private String CREATE_ISSUES_URL = System.getenv("BASE_URL") + "secure/CreateIssue!default.jspa";
+
+    @BeforeEach
+    public void setUp(){
+        driver = new ChromeDriver();
+        loginPage = new LoginPage(driver);
+        driver.get(LOGIN_URL);
+        loginPage.handleLogin();
+        createIssuesPage = new CreateIssuesPage(driver);
+        driver.manage().window().maximize();
+        driver.get(CREATE_ISSUES_URL);
+    }
+
+    @AfterEach
+    public void tearDown(){
+        driver.quit();
+    }
+
+    @Test
+    public void testH1CreateIssueIsDisplayed() {
+       Assertions.assertTrue(createIssuesPage.isCreateIssueH1Displayed(), "The h1 element with text 'Create Issue' is not displayed.");
+    }
+
 }
